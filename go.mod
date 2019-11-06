bbff2034b1fc6288affcb0e5452228c8f189ed05
Alex Dzyoba <alex@dzyoba.com>
2019-11-06 22:35:14 +0300

    Initial

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/alexdzyoba/counter64.io
+
+go 1.13
+
+require (
+	github.com/alexdzyoba/counter64 v0.0.2
+	github.com/gin-gonic/gin v1.4.0
+)
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/alexdzyoba/counter64"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	done := make(chan bool)
+	counter := counter64.New()
+	go counter.Count(done)
+
+	router := gin.Default()
+	router.LoadHTMLFiles("templates/index.tmpl")
+	router.GET("/", func(c *gin.Context) {
+		val := counter.Read()
+		dec := fmt.Sprintf("%020d", val)
+		hex := fmt.Sprintf("0x%016x", val)
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"decimal":     dec,
+			"hexademical": hex,
+		})
+	})
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %v\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("shutdown server")
+
+	done <- true
+	ctx := context.Background()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("server shutdown error: %v", err)
+	}
+}
