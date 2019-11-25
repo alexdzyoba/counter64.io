5a9349508b80b58992847a162e4176f34dd6e700
Alex Dzyoba <alex@dzyoba.com>
2019-11-25 23:35:21 +0300

    infra: Add VPC resources (igw, subnet, route table)

--- infra/main.tf
+++ infra/main.tf
@@ -6,3 +6,28 @@ provider "aws" {
 resource "aws_route53_zone" "primary" {
   name = "counter64.io"
 }
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.0.0/24"
+}
+
+resource "aws_route_table" "internet" {
+  vpc_id = "${aws_vpc.main.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.main.id}"
+  }
+}
