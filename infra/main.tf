37a1361775bfb5484cb1222fc0eaa25b5d02a567
Alex Dzyoba <alex@dzyoba.com>
2020-01-26 08:24:03 +0300

    infra: Create route in main route table instead of creating one

--- infra/main.tf
+++ infra/main.tf
@@ -20,10 +20,9 @@ resource "aws_subnet" "public" {
   cidr_block = "10.0.0.0/24"
 }
 
-resource "aws_route_table" "internet" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
+resource "aws_route" "internet" {
+  route_table_id         = aws_vpc.main.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
   }
 }
